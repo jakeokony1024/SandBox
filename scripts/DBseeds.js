7651158058194eeea227a8e5c09359642165a19e
Jakeokony1024 <jakeokony@gmail.com>
2020-04-23 21:06:53 -0500

    started user model and basic routes

---
+++ scripts/DBseeds.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const db = require("../models");
+
+//This file empties the Items & Users collections and inserts the items & users below
+
+mongoose.connect(
+	process.env.MONGODB_URI || "mongodb://localhost:27017/SandBox",
+	{ useNewUrlParser: true }
+);
+
+const userSeed = [
+	{
+		note: "Buy milk",
+		author: "Nicole",
+		date: new Date(Date.now()),
+	},
+	{
+		note: "Walk the dog",
+		author: "Sam",
+		date: new Date(Date.now()),
+	},
+];
+
+db.Item.remove({})
+	.then(() => db.Item.collection.insertMany(itemSeed))
+	.then((data) => {
+		console.log(data.result.n + " records inserted!");
+	})
+	.catch((err) => {
+		console.error(err);
+		process.exit(1);
+	});
