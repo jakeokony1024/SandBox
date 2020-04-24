17ee327a25a21b841ce67d5ad8b136d2ac5b978b
Jakeokony1024 <jakeokony@gmail.com>
2020-04-23 21:20:13 -0500

    added dotenv file with api keys

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require("express");
-
 const mongoose = require("mongoose");
 // const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3000;
+require('dotenv').config()
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
